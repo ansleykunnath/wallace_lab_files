a3a2bca94180fcf2f1c0a0b6845117bb6d0fb34a
kimas8 <164533816+kimas8@users.noreply.github.com>
2024-03-22 11:56:00 -0500

    Add files via upload

---
+++ picked_points.pp
@@ -0,0 +1,34 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime time="11:43:06" date="2024-03-08"/>
+  <User name="neuro"/>
+  <DataFileName name="Model.obj"/>
+  <templateName name=""/>
+ </DocumentData>
+ <point y="-0.16768892" x="0.17087479" name="0" z="-0.3363573" active="1"/>
+ <point y="-0.19827963" x="0.15405947" name="rpa" z="-0.34124172" active="1"/>
+ <point y="-0.23182242" x="0.15630072" name="rpa" z="-0.33817497" active="1"/>
+ <point y="-0.14467035" x="0.20259953" name="rpa" z="-0.36797947" active="1"/>
+ <point y="-0.16949281" x="0.17162581" name="rpa" z="-0.37084875" active="1"/>
+ <point y="-0.20130405" x="0.15723391" name="rpa" z="-0.36434007" active="1"/>
+ <point y="-0.16116981" x="0.20874557" name="rpa" z="-0.40000206" active="1"/>
+ <point y="-0.18155095" x="0.18225344" name="rpa" z="-0.39929891" active="1"/>
+ <point y="-0.22175866" x="0.18517797" name="rpa" z="-0.39803588" active="1"/>
+ <point y="-0.24238086" x="0.16367121" name="rpa" z="-0.39731306" active="1"/>
+ <point y="-0.17715587" x="0.24565889" name="nas" z="-0.42849129" active="1"/>
+ <point y="-0.21202388" x="0.23789823" name="nas" z="-0.44276002" active="1"/>
+ <point y="-0.25189075" x="0.23370241" name="nas" z="-0.44190559" active="1"/>
+ <point y="-0.18065467" x="0.27452755" name="nas" z="-0.42694002" active="1"/>
+ <point y="-0.21738437" x="0.27134603" name="nas" z="-0.44049042" active="1"/>
+ <point y="-0.25461325" x="0.26950049" name="nas" z="-0.44027132" active="1"/>
+ <point y="-0.16876464" x="0.3184486" name="lpa" z="-0.38835475" active="1"/>
+ <point y="-0.20125368" x="0.33660257" name="lpa" z="-0.38596857" active="1"/>
+ <point y="-0.22947949" x="0.34281406" name="lpa" z="-0.38333091" active="1"/>
+ <point y="-0.15847814" x="0.31903473" name="lpa" z="-0.36245662" active="1"/>
+ <point y="-0.18958251" x="0.34129921" name="lpa" z="-0.35066268" active="1"/>
+ <point y="-0.22311155" x="0.34407741" name="lpa" z="-0.35448626" active="1"/>
+ <point y="-0.1823751" x="0.3399328" name="lpa" z="-0.32062092" active="1"/>
+ <point y="-0.22202052" x="0.34879574" name="lpa" z="-0.31783351" active="1"/>
+ <point y="-0.24954034" x="0.34397188" name="lpa" z="-0.31952596" active="1"/>
+</PickedPoints>
